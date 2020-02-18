04d12abdeac9bbfc69e6e6f19eb714a09a8c535b
Gregory McMullan <gregory.mcmullan@mustangs>
2020-02-18 10:52:15 -0700

    more chance work

--- Monopoly Project/src/Directions.java
+++ Monopoly Project/src/Directions.java
@@ -1,9 +1,29 @@
 
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Directions
 	{
-		
-		public static void howToPlay()
+	static JFrame frame = new JFrame();
+		public static void howToPlay() throws FileNotFoundException, IOException 
 		{
+			
+			try(BufferedReader bufferedReader = new BufferedReader(new FileReader("MonopolyDirections.txt")))
+			{
+				String line;
+				while((line = bufferedReader.readLine()) != null)
+				{
+					JOptionPane.showMessageDialog(frame, line);
+				}
+			}
+
 			
 		}
 
--- Monopoly Project/src/PayCard.java
+++ Monopoly Project/src/PayCard.java
@@ -5,6 +5,11 @@ import javax.smartcardio.CardException;
 
 public class PayCard extends Card
 	{
-
+private String text;
+private int amount;
+public PayCard(String text,int value) {
+	this.text=text;
+	this.amount=value;
+}
 
 	}
