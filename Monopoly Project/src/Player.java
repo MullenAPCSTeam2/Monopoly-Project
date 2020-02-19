de1753bc97acc721946ae5ea04b4e0b5ae2d4aae
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2020-02-19 11:48:16 -0700

    commit

--- Monopoly Project/src/Player.java
+++ Monopoly Project/src/Player.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-
-public class Player 
-{
-	private static int money;
-	private static String name;
-	protected int   movementDirection;
-	
-	public Player(int m, String n)
-	{
-		setMoney(m);
-		setName(n);
-		this.movementDirection = 1;
-	}
-
-	public static int getMoney() {
-		return money;
-	}
-
-	public static void setMoney(int money) {
-		Player.money = money;
-	}
-
-	public static String getName() {
-		return name;
-	}
-
-	public static void setName(String name) {
-		Player.name = name;
-	}
-	
-	public void switchDirection(){
-		this.movementDirection *= -1;
-	}
-}
-=======
-
 public class Player 
 {
 	private static int money;
@@ -81,4 +44,4 @@ public class Player
 		this.movementDirection *= -1;
   }
 }
->>>>>>> upstream/master
+
