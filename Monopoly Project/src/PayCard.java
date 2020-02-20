297f80faf0ee43f1bf5ecfecaebf9fc65185e645
gregorymcmullan <54373118+gregorymcmullan@users.noreply.github.com>
2020-02-20 11:15:11 -0700

    intro finished i think (#46)

--- Monopoly Project/src/PayCard.java
+++ Monopoly Project/src/PayCard.java
@@ -3,13 +3,20 @@
 public class PayCard extends Card
 	{
 
-private String text;
-private int amount;
-public PayCard(String text,int value) {
-	super();
-	this.text=text;
-	this.amount= value;
-}
+//private String text;
+//private int amount;
+//public PayCard(String text,int value) {
+//	super();
+//	this.text=text;
+//	this.amount= value;
+//	
+//}
+//public void execute(Player p) { // supposed to add money after getting certain card
+//	p.addMoney(amount);
+//}
+//public String printText() { //prints text
+//	return this.text;
+//}
 }
 
 
