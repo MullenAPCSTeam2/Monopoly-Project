9bd37f9e5009c6633366c275ff371a85a88a037e
LaurenSciullo <laurensciullo29@gmail.com>
2020-02-23 22:31:46 -0700

    Purchase Property (#82)
    You can now purchase property!

--- Monopoly Project/src/PurchasableTile.java
+++ Monopoly Project/src/PurchasableTile.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public abstract class PurchasableTile extends Tile
 	{
@@ -8,23 +10,41 @@ public abstract class PurchasableTile extends Tile
 		protected boolean purchased;
 		protected Player owner;
 		protected int cost;
+		static Scanner userInput = new Scanner(System.in);
+		static ArrayList<Player> players = new ArrayList<Player>();
+		static Player currentPlayer;
+		static int choice;
 		public PurchasableTile(String tileName, int cost) {
 			super(tileName);
-		this.purchased=false;
+		//this.purchased=false;
 		this.cost=cost;
 		}
 		
 		@Override
-		public void landOnTile(){			
-			if(purchased = false) {
-			System.out.println("would you like to buy " + tileName + " it costs " + cost);
-			}
-			else {
-				System.out.println("You do not own this tile someone else does you owe them rent");
-			}
+
+		public void landOnTile(){
+			if(purchased == false) {
+			System.out.println("You landed on: " + tileName);
+			System.out.println("Would you like to buy " + tileName + "? It costs: $" + cost +
+					"\n1.Yes 2.No");
+			choice = userInput.nextInt();
 			
+			if (choice == 1)
+			{
+				if (Runner.currentPlayer.getMoney() >= cost)
+				{
+					owner = Runner.currentPlayer;
+					purchased = true;
+					Runner.currentPlayer.addMoney(cost*-1);
+					System.out.println("You have bought: " + tileName + "\nYou now have: $" +  Runner.currentPlayer.getMoney());
+				}
+        else
+        {
+        System.out.println("You do not have enough money to buy " + tileName);
+        }
+        }
+
+		
 		}
-		
-		
 		
 	}
